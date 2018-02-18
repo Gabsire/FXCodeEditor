a3717070e9f802b1dfcbc324823c4100570fd503
Gabriel Cyr <cyr.gabriel@hotmail.com>
2018-02-18 15:56:22 -0500

    load method implemented in main controller

--- src/com/editor/controller/MainController.java
+++ src/com/editor/controller/MainController.java
@@ -6,6 +6,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
@@ -14,42 +17,56 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainController {
-	
+
 	@FXML
 	TextArea textArea;
-	
+
 	@FXML
-	public void save() throws Exception{
+	public void save() throws Exception {
 		FileChooser fileChooser = new FileChooser();
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("HTML files (*.html)", "*.html");
-		fileChooser.getExtensionFilters().add(extFilter);
+		fileChooser.setTitle("Save file");
+		FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("HTML files (*.html)", "*.html");
+		fileChooser.getExtensionFilters().add(extensionFilter);
 		File file = fileChooser.showSaveDialog(null);
-		
+
 		BufferedReader reader = new BufferedReader(new StringReader(textArea.getText()));
 		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
 
 		reader.lines().forEach(s -> {
-				try {
-					writer.write(s);
-					writer.newLine();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		});	
+			try {
+				writer.write(s);
+				writer.newLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 		reader.close();
 		writer.close();
-		
-		
+
 	}
-	
+
 	@FXML
-	public void load(){
+	public void load() throws IOException {
 		
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Load file");
+		FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("HTML files (*.html)", "*.html");
+		fileChooser.getExtensionFilters().add(extensionFilter);
+		File file = fileChooser.showOpenDialog(null);
+		
+		if (null != textArea && null != file) {
+
+			StringBuilder content = new StringBuilder();
+			BufferedReader r = Files.newBufferedReader(Paths.get(file.getPath()), StandardCharsets.UTF_8);
+			r.lines().forEach(s -> content.append(s).append("\n"));
+			textArea.setText(content.toString());
+		}
+
 	}
-	
+
 	@FXML
-	public void close(){
-		
+	public void close() {
+
 	}
 
 }
