b8987e913b02613e19a7af3c0f066ba5fb6db115
Gabriel Cyr <cyr.gabriel@hotmail.com>
2018-02-18 21:40:52 -0500

    css file and more gitignore

--- src/CodeEditorApplication.java
+++ src/CodeEditorApplication.java
@@ -10,7 +10,7 @@ public class CodeEditorApplication extends Application {
 
 	@FXML
 	TextArea rowNumbers;
-	
+
 	public static void main(String[] args) {
 		Application.launch(CodeEditorApplication.class, args);
 	}
@@ -19,7 +19,6 @@ public class CodeEditorApplication extends Application {
 	public void start(Stage stage) throws Exception {
 
 		Parent root = FXMLLoader.load(getClass().getResource("editorMainWindow.fxml"));
-       
 
 		stage.setTitle("FXCodeEditor");
 		Scene scene = new Scene(root, 1000, 800);
