a279cfdf88895360f0ee544670ccdc6018614412
Gabriel Cyr <cyr.gabriel@hotmail.com>
2018-02-21 21:33:40 -0500

    tied searchbar to main window and put the logic in SearchService

--- src/com/editor/controller/MainController.java
+++ src/com/editor/controller/MainController.java
@@ -6,12 +6,14 @@ import org.controlsfx.control.textfield.CustomTextField;
 import org.fxmisc.richtext.CodeArea;
 
 import com.editor.service.FileOperationsService;
+import com.editor.service.SearchService;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ToolBar;
 
 public class MainController {
 
@@ -19,20 +21,25 @@ public class MainController {
 
 	@FXML
 	private TextArea rowNumbers;
-	
+
 	@FXML
 	private Button closeSearchBtn;
-	
+
 	@FXML
 	private Label itemsFoundLabel;
-	
+
+	@FXML
+	private ToolBar searchBar;
+
 	@FXML
 	private CustomTextField searchField;
-	
+
 	@FXML
-	SearchController searchController = new SearchController();
+	private Label searchLabel;
 
 	FileOperationsService fileOperationsService = new FileOperationsService();
+	
+	SearchService searchService = new SearchService();
 
 	@FXML
 	public void save() {
@@ -57,6 +64,16 @@ public class MainController {
 		Platform.exit();
 	}
 
+	@FXML
+	public void displaySearchBar(){
+		searchService.display(searchBar);
+	}
+	
+	@FXML
+	public void closeSearchBar() {
+		searchService.close(searchBar);
+	}
+
 	public void handleIOException(String message, StackTraceElement[] stackTraceElements) {
 		System.out.println(message + stackTraceElements.toString());
 	}
@@ -67,7 +84,6 @@ public class MainController {
 
 	public void setCodeArea(CodeArea codeArea) {
 		this.codeArea = codeArea;
-		searchController.setCodeArea(codeArea);
 	}
 
 }
---
+++ src/com/editor/service/SearchService.java
@@ -0,0 +1,16 @@
+package com.editor.service;
+
+import javafx.scene.control.ToolBar;
+
+public class SearchService {
+
+	public void close(ToolBar searchBar){
+		searchBar.managedProperty().bind(searchBar.visibleProperty());
+		searchBar.setVisible(false);
+	}
+	
+	public void display(ToolBar searchBar){
+		searchBar.managedProperty().bind(searchBar.visibleProperty());
+		searchBar.setVisible(true);
+	}
+}
